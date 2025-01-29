20aabcb9a4ca13627fed4c274b4fd4889da049cb
SANTIAGO RAMÍREZ FORERO <57204688+SANTIAGORF24@users.noreply.github.com>
2025-01-29 11:14:41 -0500

    Update Banner.js

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -28,17 +28,12 @@ export function Banner() {
             <div className="max-w-md py-5 md:py-10">
               <div className="space-y-1">
                 <h4 className="text-medium font-medium ">Segunda entrega</h4>
-                <p className="text-small text-default-400">
-                  Edwin Julian Acuña Gomez
-                </p>
+              
               </div>
               <Divider className="my-2 md:my-4" />
               <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 text-small">
                 <div>Santiago Ramírez</div>
                 <Divider orientation="vertical" className="hidden md:block" />
-                <div>Jonatan Gómez</div>
-                <Divider orientation="vertical" className="hidden md:block" />
-                <div>Sean Osorio</div>
               </div>
             </div>
           </div>
